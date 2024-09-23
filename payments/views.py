38602bb8158c46cd531edab6ffd18e702e153a6e
Roman Shupak <romanshupak808@gmail.com>
2024-09-23 12:39:36 +0300

    Create views for payments and make sure non-admins can see only their Payments when admins can see all of them.

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+
+        user = self.request.user
+        if not user.is_staff:
+            return Payment.objects.filter(borrowing__user=self.request.user)
+        return Payment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
