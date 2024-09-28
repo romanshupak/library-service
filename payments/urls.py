d883c01b9c07a24af9cee2f88cb315f59a8aa3bb
Roman Shupak <romanshupak808@gmail.com>
2024-09-28 17:45:43 +0300

    Add webhook, success and cancel paths. Modify create_checkout_session path.

--- payments/urls.py
+++ payments/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from payments.views import PaymentViewSet
+from payments.views import (
+    PaymentViewSet,
+    CreateCheckoutSessionView,
+    stripe_webhook, success_view, cancel_view
+)
 
 router = routers.DefaultRouter()
 router.register("payments", PaymentViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "create_checkout_session/<int:pk>/",
+        CreateCheckoutSessionView.as_view(),
+        name="payment-create-checkout-session"
+    ),
+    path("webhook/", stripe_webhook, name="stripe-webhook"),
+    path("success/", success_view, name="success"),
+    path("cancel/", cancel_view, name="cancel"),
+]
 
 app_name = "payments"
