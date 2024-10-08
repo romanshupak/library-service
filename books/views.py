4f15cb458101cde0e4c8fb02439d11ecba6f6639
Roman Shupak <romanshupak808@gmail.com>
2024-10-08 19:45:48 +0300

    Create tests for book`s application.

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from books.models import Book
