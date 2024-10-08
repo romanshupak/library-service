357af07c705b0a9fccd50d185444816ed025f29a
Roman Shupak <romanshupak808@gmail.com>
2024-10-08 12:13:38 +0300

    Add creation of payment in db during fine payment.

--- payments/models.py
+++ payments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from borrowings.models import Borrowing
+# from borrowings.models import Borrowing
 
 
 class Payment(models.Model):
@@ -21,7 +21,7 @@ class Payment(models.Model):
         max_length=7,
         choices=PaymentType.choices,
     )
-    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    borrowing = models.ForeignKey("borrowings.Borrowing", on_delete=models.CASCADE)
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=8, decimal_places=2)
