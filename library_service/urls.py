72b51c405184ae735555c124d80386069a5219ea
Roman Shupak <romanshupak808@gmail.com>
2024-10-08 18:31:21 +0300

    Add swagger, redoc and debug paths in urlpatterns.

--- library_service/urls.py
+++ library_service/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,4 +39,16 @@ urlpatterns = [
         include("payments.urls", namespace="payments")
     ),
     path("api/user/", include("users.urls", namespace="users")),
-]
+    path("api/media_api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
