1fc9750def334009edb7332d6ec43d99dc55f308
Roman Shupak <romanshupak808@gmail.com>
2024-10-11 13:21:19 +0300

    Hide secret key var.

--- library_service/settings.py
+++ library_service/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@04zpohjwu1s%c($$7^dlgq)3y@#d#k5^+zet5j04_lh)!f59j"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
